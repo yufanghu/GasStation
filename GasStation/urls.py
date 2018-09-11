181072fb8bab4f372ca08de171f18eea583975d3
weili.guo <weili.guo@yintech.cn>
2018-09-11 17:45:47 +0800

    搭建管理员登陆页面框架

--- GasStation/urls.py
+++ GasStation/urls.py
@@ -25,4 +25,5 @@ from django.urls import path
 urlpatterns = [
     #path('admin/', admin.site.urls),
 	path('login/', view.login),
+    path('index/', view.index),
 ]
--- GasStation/view.py
+++ GasStation/view.py
@@ -5,3 +5,6 @@ def login(request):
     context          = {}
     context['hello'] = 'Hello World!'
     return render(request, 'login.html', context)
+def index(request):
+    context          = {}
+    return render(request, 'index.html', context)
