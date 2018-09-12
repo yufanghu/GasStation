9ad87b8373c1990ab766a54435bf4e82d40cca3a
weili.guo <weili.guo@yintech.cn>
2018-09-12 19:12:31 +0800

    writecode

--- GasStation/view.py
+++ GasStation/view.py
@@ -2,9 +2,15 @@
 from django.shortcuts import render
  
 def login(request):
-    context          = {}
-    context['hello'] = 'Hello World!'
-    return render(request, 'login.html', context)
+    # context          = {}
+    # context['hello'] = 'Hello World!'
+    # if request.method=='GET':
+    #     return render(request, 'index.html')
+    # else
+    #     name=request.POST.get('user')
+    #     pwd=request.POST.get('pwd')
+
+    return render(request, 'login.html')
 def index(request):
     context          = {}
     return render(request, 'index.html', context)
